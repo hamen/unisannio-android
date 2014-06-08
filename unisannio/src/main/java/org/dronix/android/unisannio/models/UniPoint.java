bd0de5445108d0d6b3f4283992158aa9b62d2de6
Ivan Morgillo <imorgillo@gmail.com>
2014-06-08 11:27:15 +0200

    Add Parcelable to Unipoint

--- unisannio/src/main/java/org/dronix/android/unisannio/models/UniPoint.java
+++ unisannio/src/main/java/org/dronix/android/unisannio/models/UniPoint.java
@@ -2,12 +2,15 @@ package org.dronix.android.unisannio.models;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Data
 @Accessors(prefix = "m")
-public class UniPoint {
+public class UniPoint implements Parcelable {
 
     private String mFaculty;
 
@@ -29,4 +32,38 @@ public class UniPoint {
         mLng = lng;
         mGeopoint = new LatLng((int) (lat * 1E6), (int) (lng * 1E6));
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.mFaculty);
+        dest.writeString(this.mName);
+        dest.writeString(this.mAddress);
+        dest.writeDouble(this.mLat);
+        dest.writeDouble(this.mLng);
+        dest.writeParcelable(this.mGeopoint, flags);
+    }
+
+    private UniPoint(Parcel in) {
+        this.mFaculty = in.readString();
+        this.mName = in.readString();
+        this.mAddress = in.readString();
+        this.mLat = in.readDouble();
+        this.mLng = in.readDouble();
+        this.mGeopoint = in.readParcelable(LatLng.class.getClassLoader());
+    }
+
+    public static Parcelable.Creator<UniPoint> CREATOR = new Parcelable.Creator<UniPoint>() {
+        public UniPoint createFromParcel(Parcel source) {
+            return new UniPoint(source);
+        }
+
+        public UniPoint[] newArray(int size) {
+            return new UniPoint[size];
+        }
+    };
 }
